bb46ba6b2e37f005661688a94f12d13a332bf491
Nicholas Lee <nicholas.lee21@ycdsbk12.ca>
2020-12-05 00:03:10 +0000

    Passed Test Problem 1

--- src/recursionAssignment/Recursion.java
+++ src/recursionAssignment/Recursion.java
@@ -1,5 +1,15 @@
 package recursionAssignment;
 
-public class Recursion{
+import java.io.*;
 
+public class Recursion{
+  public static int countHi(String str){
+    if (str.length() == 0 || str.length() == 1) {
+      return 0;
+    } else if(str.charAt(0) == 'h' && str.charAt(1) == 'i'){
+      return 1 + Recursion.countHi(str.substring(1));
+    } else {
+      return 0 + Recursion.countHi(str.substring(1));
+    }
+  }
 }
