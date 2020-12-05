2bb88c01880e0283b96f7da6334f3bea5418d824
Nicholas Lee <nicholas.lee21@ycdsbk12.ca>
2020-12-05 00:15:57 +0000

    Refactor Code and Comments Problem 1

--- src/recursionAssignment/Recursion.java
+++ src/recursionAssignment/Recursion.java
@@ -3,13 +3,32 @@ package recursionAssignment;
 import java.io.*;
 
 public class Recursion{
+
+  /**
+  * Given a string, recursively counts the number of times "hi" appears in the string
+  * 
+  * @param str The string that is being read 
+  * @return The number of times "hi" appears in the string
+  */
   public static int countHi(String str){
+
+    // Base Case
+    // If there is only one or zero characters left, the program returns 0
     if (str.length() == 0 || str.length() == 1) {
       return 0;
-    } else if(str.charAt(0) == 'h' && str.charAt(1) == 'i'){
+    } 
+    
+    // Recursive Call 
+    // Looks for the characters "hi" in the string
+    else if(str.charAt(0) == 'h' && str.charAt(1) == 'i') {
       return 1 + Recursion.countHi(str.substring(1));
-    } else {
+    } 
+    
+    else {
       return 0 + Recursion.countHi(str.substring(1));
     }
+
   }
+
+
 }
--- src/recursionAssignment/RecursionTest.java
+++ src/recursionAssignment/RecursionTest.java
@@ -1,4 +1,5 @@
 package recursionAssignment;
+
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
@@ -6,18 +7,23 @@ import org.junit.jupiter.api.Test;
 import recursionAssignment.Recursion;
 
 public class RecursionTest{
-  /**
-    * An initial test
-    */
+
   @Test
   public void Test1(){
-    // make assertion statement(s)
-    // assertEquals(EXPECTEDVALUE, TESTVALUE);
-    // assertTrue(TESTBOOLEAN);
     assertEquals(1, Recursion.countHi("xxhixx"));
   }
+
+  @Test
+  public void Test2(){
+    assertEquals(2, Recursion.countHi("xhixhix"));
+  }
+
+  @Test
+  public void Test3(){
+    assertEquals(0, Recursion.countHi("xuuuu"));
+  }
   
-  // add more tests
+
     
 
     
