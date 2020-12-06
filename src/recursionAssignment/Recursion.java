48e7f887deabbfc56365aa26896706c6db10cc10
Nicholas Lee <nicholas.lee21@ycdsbk12.ca>
2020-12-06 01:44:55 +0000

    Minor Formatting Edits

--- src/recursionAssignment/Recursion.java
+++ src/recursionAssignment/Recursion.java
@@ -9,6 +9,7 @@ public class Recursion{
   * 
   * @param str The string that is being read 
   * @return The number of times "hi" appears in the string
+  * @author N.Lee
   */
   public static int countHi(String str){
 
@@ -18,21 +19,25 @@ public class Recursion{
       return 0;
     } 
     
-    // Recursive Call 
-    // Looks for the characters "hi" in the string
-    else if(str.charAt(0) == 'h' && str.charAt(1) == 'i') {
+    // Recursive Case 
+    // Looks for the characters "h" and "i" in the first and second characters of the string
+    if (str.charAt(0) == 'h' && str.charAt(1) == 'i') {
+      // Returns value of 1 and the substring of the string after the first character
       return 1 + Recursion.countHi(str.substring(1));
     } else {
+      // Returns 0 and the substring of the string after the first character
       return 0 + Recursion.countHi(str.substring(1));
     }
 
   }
 
+
   /**
-  * Given a string, recursively replaces all "pi" found in the string to "3.14"
+  * Given a string, recursively replaces all appearances of "pi" found in the string to "3.14"
   * 
   * @param str The string that is being read 
-  * @return The new string 
+  * @return The new string with "3.14" instead of "pi" 
+  * @author N.Lee
   */
   public static String changePi(String str){
 
@@ -42,39 +47,42 @@ public class Recursion{
       return str;
     }
 
-    // Recursive Call 
+    // Recursive Case 
     // Looks for the characters "pi" in the string and replaces with "3.14"
-    else if (str.charAt(0) == 'p' && str.charAt(1) == 'i') {
+    if (str.charAt(0) == 'p' && str.charAt(1) == 'i') {
+      // Returns "3.14" and the rest of the string after the two characters
       return "3.14" + Recursion.changePi(str.substring(2));
     } else {
-      return str.substring(0,1) + Recursion.changePi(str.substring(1));
+      // Returns the character and the rest of the string
+      return str.substring(0, 1) + Recursion.changePi(str.substring(1));
     }
 
   }
 
+
   /**
-  * Given a array of ints, recursively checks for a number that is followed by a number that is ten times its value.
+  * Given an array of ints, recursively checks for a number that is followed by a number that is ten times its value.
   * 
   * @param intNums The array of ints
   * @param index The index of the array
   * @return A boolean value  
+  * @author N.Lee
   */
   public static boolean array220(int[] intNums, int index){
     
     // Base Case
     // Returns false when the index reaches the length of the array minus one
-    if (index == intNums.length-1) {
+    if (index == intNums.length - 1) {
       return false;
     } 
 
-    // Recursive Call
+    // Recursive Case
     // Checks the following number if it is a value multiplied by ten of the previous number
     if (intNums[index + 1] == intNums[index] * 10 || array220(intNums, index + 1)) {
       return true;
     } else { 
       return false;
     }
-    
 
   }
 
--- src/recursionAssignment/RecursionTest.java
+++ src/recursionAssignment/RecursionTest.java
@@ -2,63 +2,88 @@ package recursionAssignment;
 
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
-
-
 import recursionAssignment.Recursion;
 
 public class RecursionTest{
 
+  // Tests for Problem Set 1
+
   @Test
+  // Test for "hi" in the middle of the string
   public void Test1(){
     assertEquals(1, Recursion.countHi("xxhixx"));
   }
 
   @Test
+  // Test for two "hi"s in the string
   public void Test2(){
     assertEquals(2, Recursion.countHi("xhixhix"));
   }
 
   @Test
+  // Test for a string with just "hi"
   public void Test3(){
-    assertEquals(0, Recursion.countHi("xuuuu"));
+    assertEquals(1, Recursion.countHi("hi"));
+  }
+
+  @Test
+  // Test for no "hi" in the string
+  public void Test4(){
+    assertEquals(0, Recursion.countHi("hhhh"));
   }
   
 
+  // Tests for Problem Set 2
+
   @Test
-  public void Test4(){
+  // Test for "pi" in the middle of the string
+  public void Test5(){
     assertEquals("x3.14x", Recursion.changePi("xpix"));
   }
 
   @Test
-  public void Test5(){
+  // Test for two consecutive "pi"s in the string 
+  public void Test6(){
     assertEquals("3.143.14", Recursion.changePi("pipi"));
   }
 
   @Test
-  public void Test6(){
+  // Test for "pi" as the first two characters of the string
+  public void Test7(){
     assertEquals("3.14p", Recursion.changePi("pip"));
+  }
+
+  @Test
+  // Test for no "pi" in the string
+  public void Test8(){
+    assertEquals("pppp", Recursion.changePi("pppp"));
   }
   
 
-  @Test
-  public void Test7(){
-    assertEquals(true, Recursion.array220(new int[] {1,2,20},0));
-  }
+  // Tests for Problem Set 3
 
   @Test
-  public void Test8(){
-    assertEquals(true, Recursion.array220(new int[] {3,30},0));
-  }
-
-  @Test
+  // Test for a "true" value when found in the last index of the array
   public void Test9(){
-    assertEquals(false, Recursion.array220(new int[] {1},0));
+    assertEquals(true, Recursion.array220(new int[] {1, 2, 20}, 0));
   }
 
   @Test
+  // Test for a "true" value with just two numbers in the array
   public void Test10(){
-    assertEquals(false, Recursion.array220(new int[] {1,2,3},0));
+    assertEquals(true, Recursion.array220(new int[] {3, 30}, 0));
   }
 
+  @Test
+  // Test for a "false" value with just one number in the array
+  public void Test11(){
+    assertEquals(false, Recursion.array220(new int[] {3}, 0));
+  }
+
+  @Test
+  // Test for a "false" value with three numbers in the array
+  public void Test12(){
+    assertEquals(false, Recursion.array220(new int[] {1, 2, 3}, 0));
+  }
 
 }
